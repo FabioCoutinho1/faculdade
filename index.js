f9245b72ee29c275a6dc0d8952f366d6c8a98c24
FabioFarias <fabiofarias549@gmail.com>
2024-11-14 00:52:23 -0300

    final do projeto

--- index.js
+++ index.js
@@ -19,6 +19,61 @@ const createElement = (tag, className) =>{
     return element;
 };
 
+let fisrtCard = '';
+let secundCard = '';
+
+const checkEndGame = () => {
+    const disableCard = document.querySelectorAll('.disable-card')
+
+    if(disableCard.length === 20){
+        alert("Parabens")
+    };
+};
+
+const chackCards = () =>{
+    const firstCat = fisrtCard.getAttribute('data-cat');
+    const secundCat = secundCard.getAttribute('data-cat');
+
+    if(firstCat === secundCat){
+        
+        fisrtCard.firstChild.classList.add('disable-card')
+        secundCard.firstChild.classList.add('disable-card')
+
+        fisrtCard = '';
+        secundCard = '';
+
+        checkEndGame();
+
+    }else{
+
+        setTimeout(() => {
+            fisrtCard.classList.remove('reveal-card');
+            secundCard.classList.remove('reveal-card');
+
+            fisrtCard = '';
+            secundCard = '';
+
+        }, 500);
+    };
+}
+
+const revelCard = ({target}) =>{
+
+    if(target.parentNode.className.includes('reveal-card')){
+        return;
+    };
+
+    if(fisrtCard === ''){
+        target.parentNode.classList.add('reveal-card');
+        fisrtCard = target.parentNode
+    } else if (secundCard === ''){
+
+        target.parentNode.classList.add('reveal-card');
+        secundCard = target.parentNode
+        chackCards();
+    };
+}
+
 const creatCard = (cat) =>{
     const card = createElement('section', 'card');
     const front = createElement('div', 'face front');
@@ -29,17 +84,22 @@ const creatCard = (cat) =>{
     card.appendChild(front);
     card.appendChild(back);
 
+    card.addEventListener('click', revelCard);
+    card.setAttribute('data-cat', cat)
+
     return card;
 };
 
 const loadGame = () =>{
 
-    const duplicationCats = [...cats, ...cats]
+    const duplicationCats = [...cats, ...cats];
 
-    duplicationCats.forEach((cat) =>{
+    const shuffle = duplicationCats.sort(() => Math.random() - 0.5);
+
+    shuffle.forEach((cat) =>{
         const card  = creatCard(cat);
-        grid.appendChild(card)
+        grid.appendChild(card);
     });
 };
 
-loadGame()
+loadGame();
